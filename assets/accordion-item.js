17cb58c12872d08224c046e56d215efeb2275524
Trieu Thanh <tvthanh.cod@gmail.com>
2024-12-13 16:56:34 +0700

    fix: update template with use block liquid

---
+++ assets/accordion-item.js
@@ -0,0 +1,38 @@
+class AccordionItem extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const accordionItemTrigger = this.querySelector(".accordion__item-trigger");
+
+    accordionItemTrigger &&
+      accordionItemTrigger.addEventListener("click", (e) => {
+        this.expandAccordionItem(e.target.parentElement);
+      });
+  }
+
+  expandAccordionItem(selectedItem) {
+    const accordionItems =
+      this.parentElement.querySelectorAll(".accordion__item");
+
+    accordionItems.forEach((item) => {
+      if (item !== selectedItem) {
+        item.querySelector(".angle-down")?.classList.remove("expanded");
+        item
+          .querySelector(".accordion__item-content")
+          ?.classList.remove("expanded");
+      }
+    });
+
+    const iconAngleDownEle = selectedItem.querySelector(".angle-down");
+    const accordionContentEle = selectedItem.querySelector(
+      ".accordion__item-content"
+    );
+
+    iconAngleDownEle?.classList.toggle("expanded");
+    accordionContentEle?.classList.toggle("expanded");
+  }
+}
+
+customElements.define("accordion-item", AccordionItem);
