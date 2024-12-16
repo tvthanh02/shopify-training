23e4f9840e7dbf3d73bea1518c631019f3a16433
Trieu Thanh <tvthanh.cod@gmail.com>
2024-12-16 14:46:49 +0700

    use build in custom element HTMLDetailsElement

--- assets/accordion-item.js
+++ assets/accordion-item.js
@@ -1,34 +1,29 @@
-class AccordionItem extends HTMLElement {
+class AccordionItem extends HTMLDetailsElement {
+  static observedAttributes = ["open"];
+
   constructor() {
     super();
   }
 
-  connectedCallback() {
-    const accordionItemTrigger = this.querySelector(".accordion__item-trigger");
+  attributeChangedCallback(name, _, __) {
+    if (name === "open") {
+      const isExpandedContent = this.hasAttribute("open");
+      const accordionItems =
+        this.parentElement.querySelectorAll(".accordion__item");
 
-    accordionItemTrigger &&
-      accordionItemTrigger.addEventListener("click", (e) => {
-        this.expandAccordionItem.bind(e.target.parentElement)();
+      accordionItems.forEach((item) => {
+        if (item === this) {
+          item
+            .querySelector(".angle-down")
+            ?.classList.toggle("expanded", isExpandedContent);
+          return;
+        }
+        item.hasAttribute("open") && item.removeAttribute("open");
+        item.classList.contains("expanded") &&
+          item.querySelector(".angle-down")?.classList.remove("expanded");
       });
-  }
-
-  expandAccordionItem() {
-    const accordionItems =
-      this.parentElement.querySelectorAll(".accordion__item");
-
-    const isExpandedContent = this.querySelector(
-      ".accordion__item-content"
-    )?.classList.contains("expanded");
-
-    accordionItems.forEach((item) => {
-      item
-        .querySelector(".angle-down")
-        ?.classList.toggle("expanded", item === this && !isExpandedContent);
-      item
-        .querySelector(".accordion__item-content")
-        ?.classList.toggle("expanded", item === this && !isExpandedContent);
-    });
+    }
   }
 }
 
-customElements.define("accordion-item", AccordionItem);
+customElements.define("accordion-item", AccordionItem, { extends: "details" });
